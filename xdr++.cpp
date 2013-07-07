2bf5f7545610d131f6049773a6f2d5dc8a7e14cb
Chris Rorvick <chris@rorvick.com>
2013-07-06 23:55:23 -0500

    C++ wrapper around XDR.

---
+++ xdr++.cpp
@@ -0,0 +1,138 @@
+#include "xdr++.h"
+
+#include <sstream>
+
+#include <cstdio>
+#include <arpa/inet.h>
+
+bool xdr(XDR* xdrs, std::string& s)
+{
+	char* p;
+	int32_t size;
+	char buf[BUFSIZ];
+	std::ostringstream ss;
+
+	switch (xdrs->x_op) {
+	case XDR_ENCODE:
+		p = const_cast<char*>(s.c_str());
+		size = s.length();
+		return	   xdrs->x_ops->x_putint32(xdrs, &size)
+			&& xdrs->x_ops->x_putbytes(xdrs, s.c_str(), size);
+
+	case XDR_DECODE:
+		if (!xdrs->x_ops->x_getint32(xdrs, &size))
+			return false;
+		p = buf;
+		while (size) {
+			uint32_t sz = size > sizeof (buf) ? sizeof (buf) : size;
+			if (!xdrs->x_ops->x_getbytes(xdrs, p, sz))
+				return false;
+			ss.write(p, sz);
+			size -= sz;
+		}
+		s = ss.str();
+		return true;
+
+	default:
+		break;
+	}
+
+	return false;
+}
+
+namespace {
+
+	bool_t xdr_getlong_callback(XDR* xdrs, long* lp)
+	{
+		int32_t v;
+		bool ret = static_cast<XDR_Base*>(xdrs)->get(v);
+		if (ret)
+			*lp = v;
+		return ret;
+	}
+
+	bool_t xdr_putlong_callback(XDR* xdrs, const long* lp)
+	{
+		int32_t v = *lp;
+		return static_cast<XDR_Base*>(xdrs)->put(v);
+	}
+
+	bool_t xdr_getbytes_callback(XDR* xdrs, caddr_t addr, u_int len)
+	{
+		return static_cast<XDR_Base*>(xdrs)->get(addr, len);
+	}
+
+	bool_t xdr_putbytes_callback(XDR* xdrs, const char* addr, u_int len)
+	{
+		return static_cast<XDR_Base*>(xdrs)->put(addr, len);
+	}
+
+	u_int xdr_getpostn_callback(const XDR* xdrs)
+	{
+		return static_cast<const XDR_Base*>(xdrs)->get_position();
+	}
+
+	bool_t xdr_setpostn_callback(XDR* xdrs, u_int pos)
+	{
+		return static_cast<XDR_Base*>(xdrs)->set_position(pos);
+	}
+
+	int32_t* xdr_inline_callback(XDR* xdrs, u_int len)
+	{
+		return reinterpret_cast<int32_t*>(static_cast<XDR_Base*>(xdrs)->get_inline(len));
+	}
+
+	void xdr_destroy_callback(XDR* xdrs)
+	{
+	}
+
+	bool_t xdr_getint32_callback(XDR* xdrs, int32_t* ip)
+	{
+		return static_cast<XDR_Base*>(xdrs)->get(*ip);
+	}
+
+	bool_t xdr_putint32_callback(XDR* xdrs, const int32_t* ip)
+	{
+		return static_cast<XDR_Base*>(xdrs)->put(*ip);
+	}
+
+	struct XDR::xdr_ops xdr_base_ops = {
+		&xdr_getlong_callback,
+		&xdr_putlong_callback,
+		&xdr_getbytes_callback,
+		&xdr_putbytes_callback,
+		&xdr_getpostn_callback,
+		&xdr_setpostn_callback,
+		&xdr_inline_callback,
+		&xdr_destroy_callback,
+		&xdr_getint32_callback,
+		&xdr_putint32_callback
+	};
+
+}
+
+XDR_Base::
+XDR_Base(xdr_op op)
+{
+	x_op = op;
+	x_ops = &xdr_base_ops;
+	x_public = 0;
+	x_private = 0;
+	x_base = 0;
+	x_handy = 0;
+}
+
+bool XDR_Base::
+get(int32_t& v)
+{
+	bool ret = get(reinterpret_cast<char*>(&v), sizeof (v));
+	v = htonl(v);
+	return ret;
+}
+
+bool XDR_Base::
+put(int32_t v)
+{
+	v = htonl(v);
+	return put(reinterpret_cast<char*>(&v), sizeof (v));
+}
---
+++ xdr++.h
@@ -0,0 +1,36 @@
+#ifndef XDRPP_H_included
+#define XDRPP_H_included
+
+#include <rpc/xdr.h>
+
+#include <string>
+
+inline bool xdr(XDR* xdrs, char& v) { return xdr_char(xdrs, &v); }
+inline bool xdr(XDR* xdrs, short& v) { return xdr_short(xdrs, &v); }
+inline bool xdr(XDR* xdrs, int& v) { return xdr_int(xdrs, &v); }
+inline bool xdr(XDR* xdrs, long& v) { return xdr_long(xdrs, &v); }
+inline bool xdr(XDR* xdrs, unsigned char& v) { return xdr_u_char(xdrs, &v); }
+inline bool xdr(XDR* xdrs, unsigned short& v) { return xdr_u_short(xdrs, &v); }
+inline bool xdr(XDR* xdrs, unsigned int& v) { return xdr_u_int(xdrs, &v); }
+inline bool xdr(XDR* xdrs, unsigned long& v) { return xdr_u_long(xdrs, &v); }
+inline bool xdr(XDR* xdrs, float& v) { return xdr_float(xdrs, &v); }
+inline bool xdr(XDR* xdrs, double& v) { return xdr_double(xdrs, &v); }
+
+bool xdr(XDR* xdrs, std::string&);
+
+struct XDR_Base : public XDR
+{
+	XDR_Base(xdr_op);
+
+	virtual bool get(int32_t&);
+	virtual bool put(int32_t);
+	virtual bool get(char*, size_t) = 0;
+	virtual bool put(const char*, size_t) = 0;
+	virtual size_t get_position() const = 0;
+	virtual bool set_position(size_t) = 0;
+	virtual char* get_inline(size_t) { return 0; }
+
+	virtual ~XDR_Base() {}
+};
+
+#endif  /* XDRPP_H_included */
