30170d17400ebcde7e8f10470974f933c75ea45f
Chris Rorvick <chris@rorvick.com>
2013-07-07 21:09:03 -0500

    Add support for std::vector.

--- xdr++.h
+++ xdr++.h
@@ -4,6 +4,7 @@
 #include <rpc/xdr.h>
 
 #include <string>
+#include <vector>
 
 inline bool xdr(XDR* xdrs, char& v) { return xdr_char(xdrs, &v); }
 inline bool xdr(XDR* xdrs, short& v) { return xdr_short(xdrs, &v); }
@@ -17,6 +18,39 @@ inline bool xdr(XDR* xdrs, float& v) { return xdr_float(xdrs, &v); }
 inline bool xdr(XDR* xdrs, double& v) { return xdr_double(xdrs, &v); }
 
 bool xdr(XDR* xdrs, std::string&);
+
+template <typename T>
+bool xdr(XDR* xdrs, std::vector<T>& v)
+{
+	uint32_t size;
+
+	switch (xdrs->x_op) {
+	case XDR_ENCODE:
+		size = v.size();
+		if (!xdr(xdrs, size))
+			return false;
+		for (size_t i = 0; i < size; ++i) {
+			if (!xdr(xdrs, v[i]))
+				return false;
+		}
+		return true;
+
+	case XDR_DECODE:
+		if (!xdr(xdrs, size))
+			return false;
+		v.resize(size);
+		for (size_t i = 0; i < size; ++i) {
+			if (!xdr(xdrs, v[i]))
+				return false;
+		}
+		return true;
+
+	default:
+		break;
+	}
+
+	return false;
+}
 
 template <typename T>
 inline bool xdr(XDR* xdrs, const T& v)
