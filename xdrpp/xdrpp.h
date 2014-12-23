17035cec150657199ec7d745f957c67b40ee87e6
Chris Rorvick <chris@rorvick.com>
2014-12-23 10:52:09 -0600

    Add support for T::xdr(XDR*)
    Route the encoding/decoding through a template class and have the
    primary implementation call into T::xdr(XDR*) and write specializations
    for everything else.  Actually, not *everything* yet, but eventually.

--- xdrpp/xdrpp.h
+++ xdrpp/xdrpp.h
@@ -11,88 +11,130 @@
 
 namespace xdrpp {
 
-inline bool xdr(XDR* xdrs, char& v) { return xdr_char(xdrs, &v); }
-inline bool xdr(XDR* xdrs, short& v) { return xdr_short(xdrs, &v); }
-inline bool xdr(XDR* xdrs, int& v) { return xdr_int(xdrs, &v); }
-inline bool xdr(XDR* xdrs, long& v) { return xdr_long(xdrs, &v); }
-inline bool xdr(XDR* xdrs, unsigned char& v) { return xdr_u_char(xdrs, &v); }
-inline bool xdr(XDR* xdrs, unsigned short& v) { return xdr_u_short(xdrs, &v); }
-inline bool xdr(XDR* xdrs, unsigned int& v) { return xdr_u_int(xdrs, &v); }
-inline bool xdr(XDR* xdrs, unsigned long& v) { return xdr_u_long(xdrs, &v); }
-inline bool xdr(XDR* xdrs, float& v) { return xdr_float(xdrs, &v); }
-inline bool xdr(XDR* xdrs, double& v) { return xdr_double(xdrs, &v); }
+namespace detail {
+
+	template <typename T>
+	struct xdrer
+	{
+		typedef T value_type;
+
+		static bool encode(XDR* xdrs, T& v)
+		{
+			return v.xdr(v);
+		}
+
+		static bool decode(XDR* xdrs, T& v)
+		{
+			return v.xdr(v);
+		}
+	};
+
+	template <typename T, bool_t (*f)(XDR*, T*)>
+	struct xdr_wrapper
+	{
+		typedef T value_type;
+
+		static bool encode(XDR* xdrs, T& v)
+		{
+			return f(xdrs, &v);
+		}
+
+		static bool decode(XDR* xdrs, T& v)
+		{
+			return f(xdrs, &v);
+		}
+	};
+
+	template <> struct xdrer<char>
+		: xdr_wrapper<char, xdr_char> {};
+
+	template <> struct xdrer<short>
+		: xdr_wrapper<short, xdr_short> {};
+
+	template <> struct xdrer<int>
+		: xdr_wrapper<int, xdr_int> {};
+
+	template <> struct xdrer<long>
+		: xdr_wrapper<long, xdr_long> {};
+
+	template <> struct xdrer<unsigned char>
+		: xdr_wrapper<unsigned char, xdr_u_char> {};
+
+	template <> struct xdrer<unsigned short>
+		: xdr_wrapper<unsigned short, xdr_u_short> {};
+
+	template <> struct xdrer<unsigned int>
+		: xdr_wrapper<unsigned int, xdr_u_int> {};
+
+	template <> struct xdrer<unsigned long>
+		: xdr_wrapper<unsigned long, xdr_u_long> {};
+
+	template <> struct xdrer<float>
+		: xdr_wrapper<float, xdr_float> {};
+
+	template <> struct xdrer<double>
+		: xdr_wrapper<double, xdr_double> {};
+
+}
 
 bool xdr(XDR* xdrs, std::string&);
+
+template <typename T>
+bool xdr(XDR* xdrs, T& v)
+{
+	switch (xdrs->x_op) {
+	case XDR_ENCODE: return detail::xdrer<T>::encode(xdrs, v);
+	case XDR_DECODE: return detail::xdrer<T>::decode(xdrs, v);
+	default:
+		break;
+	}
+
+	return false;
+}
 
 namespace detail {
 
 	template <typename Vector>
-	bool xdr_encode_vec(XDR* xdrs, Vector& vec)
+	struct vector_xdrer
 	{
-		uint32_t size = vec.size();
-		if (!xdr(xdrs, size))
-			return false;
-		for (auto &e : vec) {
-			if (!xdr(xdrs, e))
-				return false;
-		}
-		return true;
-	}
+		typedef Vector value_type;
 
-	template <typename Vector>
-	bool xdr_decode_vec(XDR* xdrs, Vector& vec)
-	{
-		uint32_t size;
-		if (!xdr(xdrs, size))
-			return false;
-		vec.resize(size);
-		for (auto &e : vec) {
-			if (!xdr(xdrs, e))
+		static bool encode(XDR* xdrs, Vector& vec)
+		{
+			uint32_t size = vec.size();
+			if (!xdr(xdrs, size))
 				return false;
+			for (auto &e : vec) {
+				if (!xdr(xdrs, e))
+					return false;
+			}
+			return true;
 		}
-		return true;
-	}
+
+		static bool decode(XDR* xdrs, Vector& vec)
+		{
+			uint32_t size;
+			if (!xdr(xdrs, size))
+				return false;
+			vec.resize(size);
+			for (auto &e : vec) {
+				if (!xdr(xdrs, e))
+					return false;
+			}
+			return true;
+		}
+	};
+
+	template <typename... Ts> struct xdrer<std::vector<Ts...> >
+		: vector_xdrer<std::vector<Ts...> > {};
+
+	template <typename... Ts> struct xdrer<std::deque<Ts...> >
+		: vector_xdrer<std::deque<Ts...> > {};
+
+	template <typename... Ts> struct xdrer<std::list<Ts...> >
+		: vector_xdrer<std::list<Ts...> > {};
 
 }  // namespace detail
-
-template <typename T>
-bool xdr(XDR* xdrs, std::vector<T>& vec)
-{
-	switch (xdrs->x_op) {
-	case XDR_ENCODE: return detail::xdr_encode_vec(xdrs, vec);
-	case XDR_DECODE: return detail::xdr_decode_vec(xdrs, vec);
-	default:
-		break;
-	}
-
-	return false;
-}
-
-template <typename T>
-bool xdr(XDR* xdrs, std::deque<T>& deq)
-{
-	switch (xdrs->x_op) {
-	case XDR_ENCODE: return detail::xdr_encode_vec(xdrs, deq);
-	case XDR_DECODE: return detail::xdr_decode_vec(xdrs, deq);
-	default:
-		break;
-	}
-
-	return false;
-}
-
-template <typename T>
-bool xdr(XDR* xdrs, std::list<T>& list)
-{
-	switch (xdrs->x_op) {
-	case XDR_ENCODE: return detail::xdr_encode_vec(xdrs, list);
-	case XDR_DECODE: return detail::xdr_decode_vec(xdrs, list);
-	default:
-		break;
-	}
-
-	return false;
-}
 
 template <typename T, typename U>
 bool xdr(XDR* xdrs, std::pair<T, U>& p)
@@ -103,64 +145,48 @@ bool xdr(XDR* xdrs, std::pair<T, U>& p)
 namespace detail {
 
 	template <typename Map>
-	bool xdr_encode_map(XDR* xdrs, Map& map)
+	struct map_xdrer
 	{
-		uint32_t size = map.size();
-		if (!xdr(xdrs, size))
-			return false;
-		for (auto& entry : map) {
-			typedef typename Map::key_type KeyType;
-			if (!xdr(xdrs, const_cast<KeyType&>(entry.first)))
-				return false;
-			if (!xdr(xdrs, entry.second))
-				return false;
-		}
-		return true;
-	}
+		typedef Map value_type;
 
-	template <typename Map>
-	bool xdr_decode_map(XDR* xdrs, Map& map)
-	{
-		uint32_t size;
-		if (!xdr(xdrs, size))
-			return false;
-		for (uint32_t i = 0; i < size; ++i) {
-			typename Map::key_type key;
-			if (!xdr(xdrs, key))
+		static bool encode(XDR* xdrs, Map& map)
+		{
+			uint32_t size = map.size();
+			if (!xdr(xdrs, size))
 				return false;
-			if (!xdr(xdrs, map[key]))
-				return false;
+			for (auto& entry : map) {
+				typedef typename Map::key_type KeyType;
+				if (!xdr(xdrs, const_cast<KeyType&>(entry.first)))
+					return false;
+				if (!xdr(xdrs, entry.second))
+					return false;
+			}
+			return true;
 		}
-		return true;
-	}
+
+		static bool decode(XDR* xdrs, Map& map)
+		{
+			uint32_t size;
+			if (!xdr(xdrs, size))
+				return false;
+			for (uint32_t i = 0; i < size; ++i) {
+				typename Map::key_type key;
+				if (!xdr(xdrs, key))
+					return false;
+				if (!xdr(xdrs, map[key]))
+					return false;
+			}
+			return true;
+		}
+	};
+
+	template <typename... Ts> struct xdrer<std::map<Ts...> >
+		: map_xdrer<std::map<Ts...> > {};
+
+	template <typename... Ts> struct xdrer<std::multimap<Ts...> >
+		: map_xdrer<std::multimap<Ts...> > {};
 
 }  // namespace detail
-
-template <typename T, typename U>
-bool xdr(XDR* xdrs, std::map<T, U>& map)
-{
-	switch (xdrs->x_op) {
-	case XDR_ENCODE: return detail::xdr_encode_map(xdrs, map);
-	case XDR_DECODE: return detail::xdr_decode_map(xdrs, map);
-	default:
-		break;
-	}
-
-	return false;
-}
-
-template <typename T, typename U>
-bool xdr(XDR* xdrs, std::multimap<T, U>& map)
-{
-	switch (xdrs->x_op) {
-	case XDR_ENCODE: return detail::xdr_encode_map(xdrs, map);
-	case XDR_DECODE: return detail::xdr_decode_map(xdrs, map);
-	default:
-		break;
-	}
-
-	return false;
-}
 
 struct XDR_Base : public XDR
 {
